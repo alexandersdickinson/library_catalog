a0025eeed9d2efd1f73df0637a85a8c5082134de
Alexander S. Dickinson <alexandersdickinson@gmail.com>
2015-11-09 21:08:02 -0800

    Adds, deletes, and modifies patrons.

--- app.rb
+++ app.rb
@@ -14,38 +14,55 @@ get('/') do
   erb(:index)
 end
 
-post('/') do
-  @header = "Library Catalog"
-  patron = Patron.new({:last_name => params.fetch('last-name'), :first_name => params.fetch('first-name'), :id => nil})
-  patron.save()
-  @patrons = Patron.all()
-  erb(:index)
+post('/patrons/new') do
+  @patron = Patron.new({:last_name => params.fetch('last-name'), :first_name => params.fetch('first-name'), :id => nil})
+  @patron.save()
+  @header = "#{@patron.last_name()}, #{@patron.first_name()}"
+  erb(:patron)
 end
 
-delete('/') do
+get('/search_patrons') do
+  @header = 'Search Patrons'
+  erb(:search_patrons)
+end
+
+get('/patron_search_results') do
+  @header = "Found Patrons"
+  search_criteria = {}
+  search_criteria[:last_name] = params.fetch('last-name') if params.fetch('last-name').length() > 0
+  search_criteria[:first_name] = params.fetch('first-name') if params.fetch('first-name').length() > 0
+  @patrons = Patron.search_by(search_criteria)
+  erb(:patron_results)
+end
+
+get('/patrons/:id') do
+  @patron = Patron.find(params.fetch('id').to_i())
+  @header = "#{@patron.last_name()}, #{@patron.first_name()}"
+  erb(:patron)
+end
+
+delete('/patrons/delete_success') do
   @header = "Library Catalog"
   patron = Patron.find(params.fetch('patron-id').to_i())
   patron.delete()
-  @patrons = Patron.all()
+  @message = "#{patron.last_name()}, #{patron.first_name()} successfully deleted."
   erb(:index)
 end
 
-get('/:id/edit') do
-  patron = Patron.find(params.fetch('id').to_i())
-  @header = "Edit #{patron.last_name()}, #{patron.first_name()}"
-  @id = params.fetch('id')
+get('/patrons/:id/edit') do
+  @patron = Patron.find(params.fetch('id').to_i())
+  @header = "Edit #{@patron.last_name()}, #{@patron.first_name()}"
   erb(:update_patron)
 end
 
-patch('/') do
-  @header = "Library Catalog"
-  patron = Patron.find(params.fetch('patron-id').to_i())
-  patron.update({:last_name => params.fetch('last-name'), :first_name => params.fetch('first-name')})
-  @patrons = Patron.all()
-  erb(:index)
+patch('/patrons/:id') do
+  @patron = Patron.find(params.fetch('patron-id').to_i())
+  @patron.update({:last_name => params.fetch('last-name'), :first_name => params.fetch('first-name')})
+  @header = "#{@patron.last_name()}, #{@patron.first_name()}"
+  erb(:patron)
 end
 
-get('/new_patron') do
+get('/add_patron') do
   @header = "Add Patron"
   erb(:patron_form)
 end
--- spec/library_catalog_integration_spec.rb
+++ spec/library_catalog_integration_spec.rb
@@ -14,12 +14,67 @@ describe('the patron creation path', :type => :feature) do
   end
 end
 
+describe('the patron search path', :type => :feature) do
+  it('returns patients found one criterion') do
+    patron1 = Patron.new({:last_name => "Smith", :first_name => "John", :id => nil})
+    patron2 = Patron.new({:last_name => "Smith", :first_name => "Ralph", :id => nil})
+    patron1.save()
+    patron2.save()
+    visit('/')
+    click_link('Search Patrons')
+    fill_in('last-name', :with => "Smith")
+    click_button('Search')
+    expect(page).to(have_content("Smith, John"))
+    expect(page).to(have_content("Smith, Ralph"))
+  end
+  
+  it('returns patients found by multiple criteria') do
+    patron1 = Patron.new({:last_name => "Smith", :first_name => "John", :id => nil})
+    patron2 = Patron.new({:last_name => "Smith", :first_name => "Ralph", :id => nil})
+    patron1.save()
+    patron2.save()
+    visit('/')
+    click_link('Search Patrons')
+    fill_in('last-name', :with => "Smith")
+    fill_in('first-name', :with => "John")
+    click_button('Search')
+    expect(page).to(have_content("Smith, John"))
+  end
+  
+  it('tells the user when no patrons are found') do
+    visit('/')
+    click_link('Search Patrons')
+    fill_in('last-name', :with => "Smith")
+    fill_in('first-name', :with => "John")
+    click_button('Search')
+    expect(page).to(have_content("No patrons found."))
+  end
+end
+
 describe('the patron deletion path', :type => :feature) do
-  it('deletes a patron') do
+  it('displays a message confirming patron deletion') do
     patron = Patron.new({:last_name => "Smith", :first_name => "John", :id => nil})
     patron.save()
     visit('/')
-    click_button("delete-smith-john")
+    click_link("Search Patrons")
+    fill_in('last-name', :with => "Smith")
+    click_button('Search')
+    click_link('Smith, John')
+    click_button('Delete Patron')
+    expect(page).to(have_content("Smith, John successfully deleted"))
+  end
+  
+  it('deletes the patron from the database') do
+    patron = Patron.new({:last_name => "Smith", :first_name => "John", :id => nil})
+    patron.save()
+    visit('/')
+    click_link("Search Patrons")
+    fill_in('last-name', :with => "Smith")
+    click_button('Search')
+    click_link('Smith, John')
+    click_button('Delete Patron')
+    click_link('Search Patrons')
+    fill_in('last-name', :with => "Smith")
     expect(page).not_to(have_content("Smith, John"))
   end
 end
@@ -29,11 +84,14 @@ describe('the patron edit path', :type => :feature) do
     patron = Patron.new({:last_name => "Smith", :first_name => "John", :id => nil})
     patron.save()
     visit('/')
-    click_link("update-smith-john")
+    click_link("Search Patrons")
+    fill_in('last-name', :with => "Smith")
+    click_button('Search')
+    click_link('Smith, John')
+    click_link('Edit Patron')
     fill_in('last-name', :with => "Doe")
     fill_in('first-name', :with => "Jane")
-    click_button("Update")
+    click_button('Edit Patron')
     expect(page).to(have_content("Doe, Jane"))
-    expect(page).not_to(have_content("Smith, John"))
   end
 end
